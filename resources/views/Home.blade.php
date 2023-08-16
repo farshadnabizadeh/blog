20a5d72a962ca2523b61b0f1c34a4fd9f1265f4e
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-08-15 23:57:28 -0700

    masterLayout

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+  <title>TransmissionWave</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+  @vite('resources/css/app.css')
+</head>
+<body>
+   <div class="relative w-full min-h-screen bg-[#283747]">
+   		<div class="absolute top-0 left-0 w-full min-h-screen">
+   		   <div class="w-[20%] min-h-screen bg-[#0B2949] border-r-[1px] border-white flex flex-col">
+   		   		<div class="w-full flex justify-center py-5">
+					<div class="w-[50%] h-[50%] bg-white rounded-full">
+						<img src="{{ asset('images/profileImage.png') }}" />
+					</div>
+				</div>
+				<div class="w-full h-full flex-1">
+				  @include("lists/index")
+				</div>
+   		   </div>
+   		   <div class="w-[80%] h-full"></div>
+   		</div>
+   </div>
+</body>
+</html>
---
+++ resources/views/lists/index.blade.php
@@ -0,0 +1,24 @@
+<ul class="w-full h-full">
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">4</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">5</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">6</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">7</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+	<li
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+</ul>
