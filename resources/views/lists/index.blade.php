7e515176ef5e6ee5e9c938970b8a2c6f803ce735
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-08-16 00:05:56 -0700

    view

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -1,24 +1,24 @@
 <ul class="w-full h-full">
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">3</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">4</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">4</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">5</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">5</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">6</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">6</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">7</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">7</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">3</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">3</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">3</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">3</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">3</li>
 	<li
-		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[]">3</li>
+		class="w-full py-4 bg-white text-black flex justify-center items-center text-xl mt-1 cursor-pointer hover:bg-[#2E86C1] hover:text-white">3</li>
 </ul>
