0ce86967da75da36a161a582786f52d9a018bfa6
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-08-17 04:57:33 -0700

    Responsive

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -12,9 +12,9 @@
 <body>
    <div class="relative w-full min-h-screen bg-[#283747]">
    		<div class="absolute top-0 left-0 w-full min-h-screen flex">
-   		   <div class="w-[20%] min-h-screen bg-[#0B2949] border-r-[1px] border-white flex flex-col">
+   		   <div class="lg:w-[30%] xl:w-[30%] 2xl:w-[20%] min-h-screen bg-[#0B2949] border-r-[1px] border-white flex flex-col">
    		   		<div class="w-full flex justify-center py-5">
-					<div class="w-[250px] h-[250px] bg-white rounded-full flex items-center justify-center">
+					<div class="lg:w-[150px] lg:h-[150px] xl:w-[200px] xl:h-[200px] 2xl:w-[250px] 2xl:h-[250px] bg-white rounded-full flex items-center justify-center">
 						<img src="{{ asset('images/profileImage.png') }}" />
 					</div>
 				</div>
@@ -22,11 +22,11 @@
 				  @include("lists/index",['Posts'=>$Posts,'id'=>$id]);
 				</div>
    		   </div>
-   		   <div class="w-[80%] min-h-screen">
-   		   	  <div class="w-full py-10 flex items-center px-16 text-3xl text-white">
+   		   <div class="lg:w-[70%] xl:w-[70%] 2xl:w-[80%] max-h-screen overflow-y-scroll">
+   		   	  <div class="w-full py-10 flex items-center lg:px-10 lg:text-2xl xl:px-16 xl:text-3xl text-white">
    		   	  	<span>{{ $content->title }}</span>
    		   	  </div>
-   		   	  <div class="w-full py-4 space-y-2 px-16 text-xl text-white formatting">
+   		   	  <div class="w-full py-4 space-y-2 lg:px-10 lg:text-base xl:text-lg xl:px-16 2xl:text-xl text-white formatting">
    		   	  	<?php echo($content->body); ?>
    		   	  	<pre style="border-radius: 5px;"><code class="language-css">p { color: red }</code></pre>
    		   	  </div>
